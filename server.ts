b0ce51fc71cff047e9fd469765a17e6b1d4a4aea
eze4556 <135364525+eze4556@users.noreply.github.com>
2024-05-24 12:18:16 -0300

    Update server.ts

--- server.ts
+++ server.ts
@@ -5,7 +5,7 @@ import figlet from "figlet";
 const server = Bun.serve({
   port: 3001,
   fetch(req) {
-     const body = figlet.textSync("Bun!");
+     const body = figlet.textSync("Backend de tu sistema juridico legal Funcionando!");
     return new Response(body);
     return new Response("Bun!");
   },
